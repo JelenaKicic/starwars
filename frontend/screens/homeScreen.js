b0513fcaa1dc07d7c82a8f057f2bdf3922d14002
Jelena <kicic.jelena@gmail.com>
2021-11-01 17:15:20 +0100

    Frontend home screen

---
+++ frontend/screens/homeScreen.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, TouchableOpacity, FlatList, Text, View, StyleSheet, Button } from 'react-native';
+import { SERVER_ADDRESS } from "../config/variables";
+
+const HomeScreen = ({ navigation }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [moviesShowed, setMoviesShowed] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [climates, setClimates] = useState([]);
+
+  const getMovies = async () => {
+     try {
+        setLoading(true);
+      const response = await fetch(SERVER_ADDRESS + '/movies');
+      const json = await response.json();
+      setMovies(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const getClimates = async () => {
+    try {
+    setLoading(true);   
+     const response = await fetch(SERVER_ADDRESS + '/climates');
+     const json = await response.json();
+     setClimates(json);
+   } catch (error) {
+     console.error(error);
+   } finally {
+     setLoading(false);
+   }
+ }
+
+  useEffect(() => {
+    getMovies();
+    getClimates();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {isLoading ? <ActivityIndicator/> : (
+        <View style={styles.container}>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Movies"
+              onPress={() => setMoviesShowed(true)}
+              color="#163678"
+            />
+            <Button
+              title="Climates"
+              onPress={() => setMoviesShowed(false)}
+              color="#163678"
+            />
+          </View>
+          { moviesShowed ?
+            <FlatList
+              data={movies}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                <View style={[styles.item, styles.movieItem]}>
+                    <Text style={styles.title}>{item.name}</Text>
+                </View>
+              )}
+            />
+            :
+            <FlatList
+              data={climates}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                <View style={[styles.item, styles.climateItem]}>
+                    <Text style={styles.title}>{item.type}</Text>
+                </View>
+              )}
+            />
+          }
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    padding: 24 
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+  },
+  movieItem: {
+    backgroundColor: '#cfe8e6',
+  },
+  climateItem: {
+    backgroundColor: '#f0d8d8',
+  },
+  title: {
+    fontSize: 25,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+  }
+});
+
+export default HomeScreen;
